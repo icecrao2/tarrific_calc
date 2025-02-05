2b2587c75f30e3028304770085bee771f53a3cdc
icecrao2 <icecrao2@daum.net>
2025-02-05 19:55:46 +0900

    간단한 hive client protocol 제작

---
+++ lib/data/client/hive/hive_client.dart
@@ -0,0 +1,22 @@
+
+
+
+import 'package:hive/hive.dart';
+
+class HiveClient {
+  late String _boxName;
+  late Box _box;
+
+  Future<void> setBoxName(String boxName) async {
+    await Hive.openBox(_boxName);
+    _box = Hive.box(_boxName);
+  }
+
+  Future<void> put({required String key, required dynamic value}) async {
+    await _box.put(key, value);
+  }
+
+  Future<dynamic> read({required String key}) async {
+    return await _box.get(key);
+  }
+}
